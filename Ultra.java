c5b1da1448357222d52acd91458703adb6938253
Shubhamso <shubham.soitkar@gmail.com>
2017-03-24 18:20:26 +0530

    new file:   Ultra.java

---
+++ Ultra.java
@@ -0,0 +1,12 @@
+public class Ultra{
+	
+Cement(String name){
+	System.out.println("Cement Name="+name);
+
+}
+//public class New {
+public static void main(String args[]) {
+Cement cm =new Cement("Ultratech");
+	System.out.println(cm);
+}
+}
