9f14fba28cf53256dee90cb65ac59a67e6c86049
Shubhamso <shubham.soitkar@gmail.com>
2017-03-24 18:19:22 +0530

    new file:   Puppy.java

---
+++ Puppy.java
@@ -0,0 +1,25 @@
+public class Puppy{
+public Puppy(){
+int total;
+int a=10;
+int b;
+}
+Public Puppy(String name){
+System.out.prinln("Doggie Name: "+name);
+}
+Public Puppy(Sting color, int price){}
+{
+b=price*a;
+System.out.prinln("color = "+color);
+}
+public int Print(){
+System.out.prinln("Price of puppy"+b);
+return b;
+}
+public static void main(String args[]){
+
+Puppy s1= new Puppy ("TOMMY");
+ s1.Puppy("BLACK",1);
+s1.Puppy.Print();
+}
+}
